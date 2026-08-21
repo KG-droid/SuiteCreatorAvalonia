80a064c04ca88f91940f5a28a61654d25a2ac8c3
KG-droid <97749670+KG-droid@users.noreply.github.com>
2026-08-21 10:26:12 +0100

    Updated log path for the user popup

--- SuiteCreatorAvalonia/Services/SuiteBuilder.cs
+++ SuiteCreatorAvalonia/Services/SuiteBuilder.cs
@@ -1466,7 +1466,7 @@ namespace SuiteCreatorAvalonia.Services
         {
             var config = new PopConfigDto
             {
-                LogFilePath = Path.Combine(_appSettingsControl.GetLogLocation(), $"{suiteExecCfg.BuildSettings.Manufacturer}_{suiteExecCfg.BuildSettings.Name}_{suiteExecCfg.BuildSettings.SuiteVersion}_{suiteExecCfg.BuildSettings.Revision}.log"),
+                LogFilePath = Path.Combine(_appSettingsControl.GetLogLocation(), $"{suiteExecCfg.BuildSettings.Manufacturer}_{suiteExecCfg.BuildSettings.Name}_{suiteExecCfg.BuildSettings.SuiteVersion}_{suiteExecCfg.BuildSettings.Revision}_Suite.log"),
                 CompanyLogoBackground = popupSettings.BackgroundColor?.ToString(),
                 MainText = "The following apps will be closed before the upgrade. Please save any work required before you continue.",
                 IsClosuresAppsVisible = popupSettings.LinkToProcClosures,
